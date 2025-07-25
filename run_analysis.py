870d09afb033a95a564764678fce2d5d22b2f98f
Carter Eikenbary <eikenbcr@tamu.edu>
2025-07-25 13:28:32 -0500

    update default clockspeed

--- run_analysis.py
+++ run_analysis.py
@@ -30,7 +30,7 @@ def main():
     parser.add_argument("-threshold", type=int, default=6250, help="Electron threshold for QPix reset (default: 6250).")
     parser.add_argument("-rmin", type=int, default=3, help="Minimum resets for t0 evaluation (default: 3).")
     parser.add_argument("-rmax", type=int, default=5, help="Maximum resets for t0 evaluation (default: 5).")
-    parser.add_argument("-clockspeed", type=str, default="100", help="Clock speed in MHz (default: 100).")
+    parser.add_argument("-clockspeed", type=str, default="1e-8", help="Clock speed in sec (default: 1e-8).")
     
     # Parse arguments
     args = parser.parse_args()
